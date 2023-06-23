38f70472bc122b47d5dd25d2dce1493db3ea653a
clakr <clarktolosa@gmail.com>
2023-06-23 22:02:01 +0800

    mobile done

--- presetClark.ts
+++ presetClark.ts
@@ -1,21 +1,122 @@
-import { Preset } from "unocss";
+import { Preset, PresetOptions } from "unocss";
 
-export default function presetClark(): Preset {
+export default function presetClark(options?: PresetOptions): Preset {
   return {
     name: "presetClark",
-    preflights: [
-      {
-        getCSS: () => ":root { font-size: 62.5%; }",
-      },
-    ],
     theme: {
-      colors: {},
       breakpoints: {
         tablet: "768px",
         desktop: "1440px",
       },
     },
-    shortcuts: {},
-    rules: [["bg-red", { "background-color": "red" }]],
+    shortcuts: {
+      counter:
+        "mx-auto w-5.6 h-5.6 grid place-items-center c-manatee fs-1.6 fw-900 lh-2.6 rounded-full border border-manatee/25 relative after:content-empty after:absolute after:h-8.4 after:w-.1 after:bg-manatee after:opacity-25 after:-top-8.4",
+      grid__image: "br-.8",
+    },
+    rules: [
+      [
+        /^mt-([\d.-]+)$/,
+        ([, value]) => ({
+          "margin-top": `${value}rem`,
+        }),
+      ],
+      [
+        /^mb-([\d.-]+)$/,
+        ([, value]) => ({
+          "margin-bottom": `${value}rem`,
+        }),
+      ],
+      [
+        /^my-([\d.-]+)\/([\d.-]+)$/,
+        ([, topValue, bottomValue]) => ({
+          "margin-block": `${topValue}rem ${bottomValue}rem`,
+        }),
+      ],
+      [
+        /^w-([\d.-]+)$/,
+        ([, value]) => ({
+          width: `${value}rem`,
+        }),
+      ],
+      [
+        /^h-([\d.-]+)$/,
+        ([, value]) => ({
+          height: `${value}rem`,
+        }),
+      ],
+      [
+        /^fs-([\d.-]+)$/,
+        ([, value]) => ({
+          "font-size": `${value}rem`,
+        }),
+      ],
+      [
+        /^lh-([\d.-]+)$/,
+        ([, value]) => ({
+          "line-height": `${value}rem`,
+        }),
+      ],
+      [
+        /^px-([\d.-]+)$/,
+        ([, value]) => ({
+          "padding-inline": `${value}rem`,
+        }),
+      ],
+      [
+        /^ta-([\w]+)$/,
+        ([, value]) => ({
+          "text-align": value,
+        }),
+      ],
+      [
+        /^br-([\d.-]+)$/,
+        ([, value]) => ({
+          "border-radius": `${value}rem`,
+        }),
+      ],
+      [
+        /^py-([\d.-]+)$/,
+        ([, value]) => ({
+          "padding-block": `${value}rem`,
+        }),
+      ],
+      [
+        /^top-([\d.-]+)$/,
+        ([, value]) => ({
+          top: `${value}rem`,
+        }),
+      ],
+      [
+        /^gap-([\d.-]+)$/,
+        ([, value]) => ({
+          gap: `${value}rem`,
+        }),
+      ],
+      [
+        /^ls-([\d.-]+)$/,
+        ([, value]) => ({
+          "letter-spacing": `${value}rem`,
+        }),
+      ],
+      [
+        /^pt-([\d.-]+)$/,
+        ([, value]) => ({
+          "padding-top": `${value}rem`,
+        }),
+      ],
+      [
+        /^pb-([\d.-]+)$/,
+        ([, value]) => ({
+          "padding-bottom": `${value}rem`,
+        }),
+      ],
+      [
+        "bg-mobile-footer",
+        {
+          "background-image": "url(/mobile/image-footer.jpg)",
+        },
+      ],
+    ],
   };
 }
--- uno.config.ts
+++ uno.config.ts
@@ -6,8 +6,29 @@ export default defineConfig({
     presetWind(),
     presetWebFonts({
       provider: "google",
-      fonts: {},
+      fonts: { sans: "Red Hat Display:400,700" },
     }),
-    presetClark(),
+    presetClark({
+      preflights: [
+        {
+          getCSS: () =>
+            ":root { font-size: 62.5%; font-family: 'Red Hat Display', sans-serif; }",
+        },
+      ],
+    }),
   ],
+  theme: {
+    colors: {
+      snow: "#fafafa",
+      valhalla: "#28283D",
+      manatee: "#87879D",
+      "columbia-blue": "#8FE3F9",
+      "boston-blue": "#4D96A9",
+      deluge: "#855FB1",
+      mauve: "#D9B8FF",
+    },
+  },
+  layers: {
+    presetClark: 3,
+  },
 });
